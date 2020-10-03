485f1706baea5fed2ac67c680eb8121508ce8ed7
Miokovich <60796211+Miokovich@users.noreply.github.com>
2020-10-04 00:39:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,75 +1,75 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <vector>
 
 struct Student{
-    int name_index;
-    int middle = 0;
+    std::string name_surname = " ";
+    size_t sum = 0;
 };
 
-void Merge_(Student* a, const int &beg_a, const int &mid, const int &end_a) {
-    int indexA = beg_a;
-    int indexB = mid;
-    int indexC = beg_a;
-    std::vector<Student> result;
-    result.reserve(end_a);
-    while (indexA < mid && indexB <= end_a) {
-        if (a[indexA].middle >= a[indexB].middle) {
-            result.push_back(a[indexA]);
-            ++indexC;
-            ++indexA;
-        }
-        else {
-            result.push_back(a[indexB]);
-            ++indexC;
-            ++indexB;
-        }
-    }
-    while (indexA < mid) {
-        result.push_back(a[indexA]);
-        ++indexC;
-        ++indexA;
-    }
-    while (indexB <= end_a) {
-        result.push_back(a[indexB]);
-        ++indexC;
-        ++indexB;
-    }
-    for (int i = 0; i < indexC - beg_a; ++i){
-        std::swap(a[beg_a + i], result[i]);
-    }
+template<class T>
+void push_in_result(std::vector<T> &result, T value, size_t &end_, size_t &start) {
+    result.push_back(value);
+    ++end_;
+    ++start;
 }
 
+template<class T>
+void Merge_(std::vector<T> &array_, size_t beg_a, size_t mid, size_t end_a) {
 
-void sort(Student* a, int l, int r){
-    int mid = (l + r) / 2;
-    if (l < mid) sort(a, l, mid);
-    if (mid + 1  < r) sort(a, mid + 1, r);
-    Merge_(a, l, mid + 1, r);
+    size_t start = beg_a;
+    size_t middle = mid;
+    size_t end_ = beg_a;
+
+    std::vector<T> result;
+
+    while (start < mid && middle <= end_a) {
+        if (array_[start].sum >= array_[middle].sum) {
+            push_in_result(result, array_[start], end_, start);
+        }
+        else {
+            push_in_result(result, array_[middle], end_, middle);
+        }
+    }
+    while (start < mid) {
+        push_in_result(result, array_[start], end_, start);
+    }
+    while (middle <= end_a) {
+        push_in_result(result, array_[middle], end_, middle);
+    }
+    swap_ranges(result.begin(), result.begin() + result.size(), array_.begin() + beg_a);
+}
+
+template<class T>
+void sort_descending(std::vector<T> &array_, size_t l, size_t r) {
+    size_t mid = (l + r) / 2;
+    if (l < mid) sort_descending(array_, l, mid);
+    if (mid + 1  < r) sort_descending(array_, mid + 1, r);
+    Merge_(array_, l, mid + 1, r);
 
 }
 
 
 int main() {
-    int n;
-    double inf, rus, math;
-    std::cin >> n;
-    Student* a = new Student[n];
-    std::vector<std::string> student_names;
-    student_names.reserve(n);
-    std::string temp_str_name, temp_str_surname;
-    for (int i = 0; i < n; ++i){
+    size_t num_of_stud;
+    std::cin >> num_of_stud;
+    std::vector<Student> stud_grades(num_of_stud);
+
+    for (size_t i = 0; i < num_of_stud; ++i){
+
+        std::string temp_str_name, temp_str_surname;
         std::cin >> temp_str_name >> temp_str_surname;
-        student_names.push_back(temp_str_name + " " + temp_str_surname);
+
+        stud_grades[i].name_surname = temp_str_name + " " + temp_str_surname;
+        int inf, rus, math;
         std::cin >> inf >> math >> rus;
-        a[i].middle = inf + rus + math;
-        a[i].name_index = i;
+        stud_grades[i].sum = inf + rus + math;
     }
-    sort(a, 0, n - 1);
-    for (int i = 0; i < n; ++i) {
-        std::cout << student_names[a[i].name_index] << '\n';
+
+    sort_descending(stud_grades, 0, num_of_stud - 1);
+    for (size_t i = 0; i < num_of_stud; ++i) {
+        std::cout << stud_grades[i].name_surname << '\n';
     }
-    delete[]a;
+
     return 0;
 }
