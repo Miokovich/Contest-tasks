68651fb9af9bba04e5b835d1b5e86b99470a4dd8
Miokovich <60796211+Miokovich@users.noreply.github.com>
2020-11-01 12:09:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,84 @@
 #include <list>
 #include <algorithm>
 
+class Graph {
+protected:
+	size_t vertex_count, edge_count;
+	bool is_directed;
 
-void color_graph(const std::vector<std::list<int>> &graph, std::vector<int> &color,
-                 int start, int cur_color, bool &cant_color){
+public:
+	typedef size_t vertex;
+	Graph(size_t vertex_count, bool is_directed) :
+	    vertex_count(vertex_count), is_directed(is_directed), edge_count(0) {}
+
+	size_t get_vertex_count() const {
+		return vertex_count;
+	}
+
+	size_t get_edge_count() const {
+		if (is_directed == 0) {
+			return edge_count / 2;
+		}
+		return edge_count;
+	}
+
+	bool get_direction() const {
+		return is_directed;
+	}
+
+	virtual void add_edge(const vertex& start, const vertex& finish) = 0;
+	virtual size_t get_vertex_deg(const vertex& vertex) const = 0;
+	virtual std::vector<std::vector<vertex>> get_adjList() const = 0;
+	virtual std::vector<vertex> get_neighbors(const vertex& v) const = 0;
+};
+
+
+class graph_adj_list : public Graph {
+private:
+	std::vector<std::vector<vertex>> adj_list_;
+
+public:
+	graph_adj_list(size_t vertex_count, bool is_directed) : Graph(vertex_count, is_directed), adj_list_(vertex_count + 1) {}
+
+	void add_edge(const vertex& start, const vertex& finish) override {
+		adj_list_[start].push_back(finish);
+		if (!is_directed) {
+			adj_list_[finish].push_back(start);
+		}
+		edge_count++;
+	}
+
+	size_t get_vertex_deg(const vertex& vertex) const override {
+		return adj_list_[vertex].size();
+	}
+
+	std::vector<std::vector<vertex>> get_adjList() const override {
+		return adj_list_;
+	}
+
+	std::vector<vertex> get_neighbors(const vertex& vertex) const override {
+		return adj_list_[vertex];
+	}
+
+    void remove_miltiple_edges() {
+        for (size_t i = 1; i <= vertex_count; ++i) {
+            sort(adj_list_[i].begin(), adj_list_[i].end());
+            std::unique(adj_list_[i].begin(), adj_list_[i].end());
+        }
+    }
+};
+
+
+enum colors {not_color = 0, white, black};
+
+void color_graph(const Graph &graph, std::vector<colors> &color,
+                 int start, colors cur_color, bool &cant_color){
     color[start] = cur_color;
-    for (const auto &neigh : graph[start]){
-        if (color[neigh] == 0){
-            color_graph(graph, color, neigh, 3 - cur_color, cant_color);
+    auto get_neigh = graph.get_neighbors(start);
+    for (const auto &neigh : get_neigh){
+        if (color[neigh] == not_color){
+            cur_color = (cur_color == white) ? black : white; 
+            color_graph(graph, color, neigh, cur_color, cant_color);
         } else {
             if (color[neigh] == cur_color) {
                 cant_color = true;
@@ -24,31 +95,25 @@ void color_graph(const std::vector<std::list<int>> &graph, std::vector<int> &col
 int main(){
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    size_t n, m;
-    std::cin >> n >> m;
+    size_t students_cnt, pair_students_cnt;
+    std::cin >> students_cnt >> pair_students_cnt;
 
-    std::vector<std::list<int>> list_edg;
-    list_edg.reserve(n + 1);
-    for (size_t i = 0; i <= n; ++i){
-        list_edg.push_back(std::list<int>());
+    graph_adj_list list_edges(students_cnt, false);
+    
+    std::vector<colors> color(students_cnt + 1, not_color);
+
+    for (size_t i = 0; i < pair_students_cnt; ++i){
+        size_t first_student, second_student;
+        std::cin >> first_student >> second_student;
+        list_edges.add_edge(first_student, second_student);
     }
 
-    int a, b;
-    std::vector<int> color(n + 1, 0);
-
-    for (size_t i = 0; i < m; ++i){
-        std::cin >> a >> b;
-        list_edg[a].push_back(b);
-        list_edg[b].push_back(a);
-    }
-
-    for (size_t i = 0; i <= n; ++i){
-        list_edg[i].unique();
-    }
+    list_edges.remove_miltiple_edges();
+    
     bool cant_color = false;
-    for (size_t i = 1; i <= n; ++i){
-        if (color[i] == 0) {
-            color_graph(list_edg, color, i, 1, cant_color);
+    for (size_t i = 1; i <= students_cnt; ++i){
+        if (color[i] == not_color) {
+            color_graph(list_edges, color, i, white, cant_color);
             if (cant_color) {
                 std::cout << "NO";
                 return 0;
