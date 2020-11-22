60d9451ec44e8fc723dbbafa86a3fdad3435ce35
Miokovich <60796211+Miokovich@users.noreply.github.com>
2020-11-22 16:15:47 +0200

    Update task_K.cpp

--- task_K.cpp
+++ task_K.cpp
@@ -5,16 +5,19 @@
 #include <algorithm>
 #include <queue>
 
+struct Pair {
+	long long weight;
+	Graph_weight::vertex vertex;
+};
 
-
-class Graph {
+class Graph_weight{
 protected:
 	size_t vertex_count, edge_count;
 	bool is_directed;
 
 public:
 	typedef size_t vertex;
-	Graph(size_t vertex_count, bool is_directed) :
+	Graph_weight(size_t vertex_count, bool is_directed) :
 	    vertex_count(vertex_count), is_directed(is_directed), edge_count(0) {}
 
 	size_t get_vertex_count() const {
@@ -32,18 +35,20 @@ public:
 		return is_directed;
 	}
 
+	std::vector<Pair> get_neighbors(vertex vertex) const;
+
 	virtual size_t get_vertex_deg(vertex vertex) const = 0;
 };
 
 
 
-class graph_adj_list_weight : public Graph {
+class graph_adj_list_weight : public Graph_weight {
 private:
-	std::vector<std::vector<std::pair<vertex, vertex>>> adj_list_;
+	std::vector<std::vector<Pair>> adj_list_;
 
 public:
 	graph_adj_list_weight(size_t vertex_count, bool is_directed) :
-	    Graph(vertex_count, is_directed), adj_list_(vertex_count + 1) {}
+	    Graph_weight(vertex_count, is_directed), adj_list_(vertex_count + 1) {}
 
 	void add_edge(vertex start, vertex finish, long long weight) {
 		adj_list_[start].push_back({finish, weight});
@@ -57,57 +62,56 @@ public:
 		return adj_list_[vertex].size();
 	}
 
-	std::vector<std::vector<std::pair<vertex, vertex>>> get_adjList() const {
+	std::vector<std::vector<Pair>> get_adjList() const {
 		return adj_list_;
 	}
 
-	std::vector<std::pair<vertex, vertex>> get_neighbors(vertex vertex) const {
+	std::vector<Pair> get_neighbors(vertex vertex) const {
 		return adj_list_[vertex];
 	}
 };
 
-void find_MST(const graph_adj_list_weight &graph, int &answer) {
+size_t find_MST(const Graph_weight &graph) {
     
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> next_vert;
+    std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> next_vert;
     std::vector<int> used(graph.get_vertex_count() + 1, 0);
     
     next_vert.push({0, 1}); //weight, num
-    answer = 0;
+    size_t weight_MST = 0;
 
     while (!next_vert.empty()) {
-        int curr_vert = next_vert.top().second;
-        int curr_dist = next_vert.top().first;
+        int curr_vert = next_vert.top().vertex;
+        int curr_dist = next_vert.top().weight;
         next_vert.pop();
         if (used[curr_vert]) continue;
         used[curr_vert] = 1;
-        answer += curr_dist;
+        weight_MST += curr_dist;
         auto neighbors = graph.get_neighbors(curr_vert);
         for (auto i : neighbors) {
-            if (!used[i.first]) {
-                next_vert.push({i.second, i.first});
+            if (!used[i.vertex]) {
+                next_vert.push({i.weight, i.vertex});
             }
         }
     }
+	return weight_MST;
 }
 
 
 int main(){
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    size_t n, m;
-    std::cin >> n >> m;
+    size_t num_vertex, num_edges;
+    std::cin >> num_vertex >> num_edges;
 
-    graph_adj_list_weight list_edg(n, false);
+    graph_adj_list_weight list_edges(num_vertex, false);
 
-    for (size_t i = 0; i < m; ++i){
+    for (size_t i = 0; i < num_edges; ++i){
         size_t from, to;
         long long len;
         std::cin >> from >> to >> len;
-        list_edg.add_edge(from, to, len);
+        list_edges.add_edge(from, to, len);
     }
-    int answer = 0;
-    
-    find_MST(list_edg, answer);
+    int answer = find_MST(list_edges);
     std::cout << answer;
     return 0;
 }
